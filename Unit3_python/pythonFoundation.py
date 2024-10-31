8694ce4bbb873bc1caa42ae3d5889bcbe8436df9
hikinepalmerjr <hpalmer530@boyslatin.net>
2024-10-31 12:47:01 +0000

    worked on Python Foundation and how to use Varibles

---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,34 @@
+# data types = are the fundamental building 
+# blocks of Python
+
+# integers = whole numbers 
+# strings = any characters wrapped in quotes 
+# boolean = true and false 
+
+#we output data using the print. function
+print(200 * 8)
+print('welcome to python.')
+
+()#round brackets = function
+{}#curly brackets = objects
+{}#square brackets = lists 
+
+# varibles = container for data 
+# we write varibles by first declaring/ giving it a name 
+# we use the equal sign.
+# we then give it data.
+name='Hikine'
+message = 'goodmorning'
+
+# concatenation = combining strings
+print(message,name)
+
+storyA =" I will be going to "
+loaction='The south philly supersite'
+time ='At 12pm'
+print(story,Location,time)
+
+valA=10
+valB=20
+
+print(valA=ValB)
