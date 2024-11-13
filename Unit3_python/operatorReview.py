f541454940cd9ddcdc4d00d846a3693225be272b
hikinepalmerjr <hpalmer530@boyslatin.net>
2024-11-13 13:46:56 +0000

    completed half of Operator Review assesement.

---
+++ Unit3_python/operatorReview.py
@@ -0,0 +1,27 @@
+# assigment operators- doing arithmetic after the assigment of a value.
+#assigment is primarily focused on the equal sign
+
+jeans = 200.0
+discont
+jeans *=0.25
+
+checkAmount=''
+print(jeans)
+
+# arithmeatic operators- math processes. addition, subtraction, mulitplication, and divsion
+sendMoney= 25.00
+depositAmount = 100.0
+withdrawlAmount= 0.0
+CheckingAccountBalance = 0.0
+savingsAccountBalance = 300.0
+investmentAccount = 0.0
+#print(depositAmount + savingAccountBalance)
+
+
+#track runner app arithmetic operator examples
+firstRunTime= 20.00
+secondRunTime= 18.00
+runningDifference = firstRunTime- secondRunTime
+# logical
+
+# comparison
